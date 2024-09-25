16fa4dbfce701b58ab15782de1ab3400e92b4a2c
Rajesh <rrrajesh167@gmail.com>
2024-09-25 09:52:57 +0530

    second class of the react

---
+++ second-app/src/Components/UserList/UserList.jsx
@@ -0,0 +1,24 @@
+import "./UserList.css";
+
+const UserList = () => {
+  const users = [
+    { id: 1, name: "Ashwani", age: 30 },
+    { id: 2, name: "Rajesh", age: 23 },
+    { id: 3, name: "Charan", age: 29 },
+  ];
+  return (
+    <>
+      <ul>
+        {users.map((item) => {
+          return (
+            <li key={item.id} className="list-items">
+              Welcome, {item.name} and your name is {item.age}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default UserList;
