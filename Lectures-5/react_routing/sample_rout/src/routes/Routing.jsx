74ec725e345bbc512a4ce406f376a0b87424a283
Rajesh <rrrajesh167@gmail.com>
2024-10-17 07:06:44 +0530

    added private Routing example

--- Lectures-5/react_routing/sample_rout/src/routes/Routing.jsx
+++ Lectures-5/react_routing/sample_rout/src/routes/Routing.jsx
@@ -3,11 +3,19 @@ import Home from "../pages/home/Home";
 import AboutUs from "../pages/aboutus/AboutUs";
 import ClassRoom from "../pages/classroom/ClassRoom";
 import PageNotFound from "../pages/pageNotFound/PageNotFound";
+import Dashboard from "../pages/dashboard/Dashboard";
+import Login from "../pages/login/Login";
+import PrivateRoute from "./PrivateRoute";
 
 const Routing = () => {
   return (
     <>
       <Routes>
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute element={<Dashboard />} />}
+        />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />}></Route>
         <Route path="/aboutus" element={<AboutUs />}></Route>
         <Route
